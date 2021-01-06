f891529c9cb23527c0215aa10281c436fb77b2b3
Bertalan Körmendy <korber314@gmail.com>
2021-01-06 19:02:59 +0100

    day 10 complete

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Challenge
-import DayNine
+import DayTen
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/src/challenges/daynine/input.txt"
-  let parsed = parse input :: String
+  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/src/challenges/dayten/input.txt"
+  let parsed = parse input :: [InputLine]
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
--- src/Utils.hs
+++ src/Utils.hs
@@ -1,12 +1,28 @@
 module Utils where
 
+import Text.Parsec as Parsec
+
 transpose :: [[a]] -> [[a]]
 transpose ([]:_) = []
 transpose x = map head x : transpose (map tail x)
 
 -- ^ https://stackoverflow.com/a/60380502
-replace index elem = map (\(index', elem') -> if index' == index then elem else elem') . zip [0..]
+replace index elem = zipWith (curry transform) [0 ..]
+  where
+    transform (i, e)
+      | i == index = elem
+      | otherwise = e
 
 concatRep :: Int -> String -> String
 concatRep n = concat . replicate n
 
+int :: Parsec.Parsec String () Int
+int = read <$> Parsec.many1 Parsec.digit
+
+parseLines :: Parsec.Parsec String () a -> String -> a
+parseLines parser = parse
+  where
+    parse input =
+      case Parsec.parse parser "" input of
+        Left err -> error $ show err
+        Right commands -> commands
