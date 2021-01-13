74bf1efd9b0a1a74cb362fe8d1d919b2fcce37de
Bertalan Körmendy <korber314@gmail.com>
2021-01-13 11:46:04 +0100

    update day 6 to include solution for part 2

--- src/challenges/daysix/DaySix.hs
+++ src/challenges/daysix/DaySix.hs
@@ -1,17 +1,13 @@
 {-# LANGUAGE FlexibleInstances #-}
-
 module DaySix where
 import Challenge
 import Utils (transpose)
-
 import Data.List (sort, group, sortOn)
-import Data.Ord
 
-part1 :: [String] -> String
-part1 = concatMap (take 1 . head . sortOn length . group . sort) . transpose
+solve :: (String -> Int) -> [String] -> String
+solve compareFn = concatMap (take 1 . head . sortOn compareFn . group . sort) . transpose
 
 instance Challenge [String] where
   parse = lines
-  partOne = part1
-  partTwo = show
-
+  partOne = solve (negate . length)
+  partTwo = solve length
