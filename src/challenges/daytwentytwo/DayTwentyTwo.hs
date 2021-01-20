49c54439a2babac166f2b8204c161f1e6121ce01
Bertalan Körmendy <korber314@gmail.com>
2021-01-20 22:44:31 +0100

    part 2 complete

--- src/challenges/daytwentytwo/DayTwentyTwo.hs
+++ src/challenges/daytwentytwo/DayTwentyTwo.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE FlexibleInstances #-}
 module DayTwentyTwo where
 import Text.Parsec
-import Data.List (sortOn, find)
-import qualified Data.HashMap as HM
 import Challenge
+import qualified Data.Map as Map
+import Data.List (sortOn)
 import Utils
 
 data Node = Node { coord :: (Int, Int)
@@ -11,7 +11,7 @@ data Node = Node { coord :: (Int, Int)
                  , used :: Int
                  , avail :: Int
                  , usePercent :: Int
-                 } deriving (Eq)
+                 } deriving (Eq, Show)
 
 
 -- /dev/grid/node-x0-y0     87T   71T    16T   81%
@@ -25,13 +25,33 @@ parseNode = Node  <$> ((,) <$> (string "/dev/grid/node-x" *> int) <*> (string "-
 viable :: Node -> Node -> Bool
 viable a b = used a > 0 && used a <= avail b
 
-solve :: [Node] -> Int
-solve nodes = 42 -- wip
+partOneI :: [Node] -> Int
+partOneI nodes = length [() | a <- nodes
+                            , b <- nodes
+                            ,  a /= b
+                            , viable a b]
 
+fromList :: [Node] -> [[Node]]
+fromList nodes = [[grid Map.! (x, y) | x <- [0..36]] | y <- [0..24]]
+  where
+    grid = Map.fromList (map (\node -> (coord node, node)) nodes)
+
+fromNode :: Node -> Char
+fromNode node
+  | coord node == (36, 0) = 'G'
+  | usePercent node >= 90 = '#'
+  | avail node >= 64 = '_'
+  | otherwise = '.'
+
+showGrid :: [[Node]] -> [String]
+showGrid = map (map fromNode)
+
+-- ^ part 2 inspired by reddit
+partTwoI :: [Node] -> String
+partTwoI =  unlines . showGrid . fromList
+
+-- ^ 👀 3 + 17 + 31 + 35 * 5 + 1
 instance Challenge [Node] where
   parse = parseLines (sepBy1 parseNode newline)
-  partOne nodes = show $ length [() | a <- nodes
-                                    , b <- nodes
-                                    ,  a /= b
-                                    , viable a b]
-  partTwo = show . solve
+  partOne = show . partOneI
+  partTwo = partTwoI
