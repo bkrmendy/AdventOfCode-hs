8fc7c768654742ff45c9c3561b7ef1697b3f50a4
Bertalan Körmendy <korber314@gmail.com>
2021-01-27 16:32:18 +0100

    part 2 complete

--- src/Year2015/Day19.hs
+++ src/Year2015/Day19.hs
@@ -1,27 +1,27 @@
 {-# LANGUAGE FlexibleInstances #-}
 module Year2015.Day19 where
 import Challenge
+import Data.List (sortOn)
 import Data.List.Split (splitOn)
 import qualified Data.Sequence as Seq
 import qualified Data.Set as Set
 
-data Replacement = Replacement String String
+data Replacement = Replacement { lhs :: String, rhs :: String}
 
 parseI line = Replacement a b
   where
     (a:b:_) = splitOn " => " line
 
 deplace :: [Replacement] -> [Replacement]
-deplace = map (\(Replacement f t) -> Replacement t f)
+deplace = map (\(Replacement from to) -> Replacement to from)
 
 replace :: String -> String -> String -> String
 replace str from to = to ++ drop (length from) str
 
 replacementsOf :: String -> Replacement -> [String]
 replacementsOf [] _ = []
-replacementsOf str (Replacement from to)
-  | to == "e" = ["e" | from == str]
-  | otherwise = replI str
+replacementsOf str (Replacement from "e") = ["e" | from == str]
+replacementsOf str (Replacement from to) = replI str
     where
       replI :: String -> [String]
       replI s
@@ -34,8 +34,8 @@ replacements str = foldr go Set.empty
   where
     go re se = Set.union se $ Set.fromList $ replacementsOf str re
 
-shrink :: String -> [Replacement] -> Int
-shrink str reps = shrinkI (Set.singleton str) (Seq.singleton (0, str))
+shrinkBfs :: String -> [Replacement] -> Int
+shrinkBfs str reps = shrinkI (Set.singleton str) (Seq.singleton (0, str))
   where
     shrinkI :: Set.Set String -> Seq.Seq (Int, String) -> Int
     shrinkI seen queue
@@ -46,6 +46,18 @@ shrink str reps = shrinkI (Set.singleton str) (Seq.singleton (0, str))
         theseReplacements = Set.filter (not . flip Set.member seen) $ replacements thisString reps
         nextSeen = Set.union seen theseReplacements
         nextQueue = Seq.drop 1 queue Seq.>< Seq.fromList (map ((,) (succ steps)) (Set.elems nextSeen))
+        
+tryReplace :: Replacement -> (Int, String) ->  (Int, String)
+tryReplace rep (n, str) =
+  case replacementsOf str rep of
+    []    -> (n, str)
+    (a:_) -> (n + 1, a)
+ 
+shrinkGreedy :: Int -> String -> [Replacement] -> Int
+shrinkGreedy steps "e" _    = steps
+shrinkGreedy steps str reps = shrinkGreedy (steps + n) asd reps
+  where
+    (n, asd) = foldr tryReplace (0, str) reps
 
 molecule :: String
 molecule = "CRnSiRnCaPTiMgYCaPTiRnFArSiThFArCaSiThSiThPBCaCaSiRnSiRnTiTiMgArPBCaPMgYPTiRnFArFArCaSiRnBPMgArPRnCaPTiRnFArCaSiThCaCaFArPBCaCaPTiTiRnFArCaSiRnSiAlYSiThRnFArArCaSiRnBFArCaCaSiRnSiThCaCaCaFYCaPTiBCaSiThCaSiThPMgArSiRnCaPBFYCaCaFArCaCaCaCaSiThCaSiRnPRnFArPBSiThPRnFArSiRnMgArCaFYFArCaSiRnSiAlArTiTiTiTiTiTiTiRnPMgArPTiTiTiBSiRnSiAlArTiTiRnPMgArCaFYBPBPTiRnSiRnMgArSiThCaFArCaSiThFArPRnFArCaSiRnTiBSiThSiRnSiAlYCaFArPRnFArSiThCaFArCaCaSiThCaCaCaSiRnPRnCaFArFYPMgArCaPBCaPBSiRnFYPBCaFArCaSiAl"
@@ -56,4 +68,4 @@ moleculeEx = "HOHOHO"
 instance Challenge [Replacement] where
   parse = map parseI . lines
   partOne = show . Set.size . replacements molecule
-  partTwo = show . shrink molecule . deplace
+  partTwo = show . shrinkGreedy 0 molecule . deplace . sortOn rhs
