2fcaa7c2590f8d9d5a331425b443f9373e0700b2
Bertalan Körmendy <korber314@gmail.com>
2021-01-30 00:40:37 +0100

    day 4 complete

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,12 @@
 module Main where
 
 import Challenge
-import Year2019.Day3
+import Year2019.Day4
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/3.txt"
-  let parsed = parse input :: [[Instr]]
+  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/4.txt"
+  let parsed = parse input :: (Int, Int)
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
---
+++ src/Year2019/Day4.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE FlexibleInstances #-}
+module Year2019.Day4 where
+import Challenge
+
+same is = or (zipWith (==) is (tail is))
+increase is = and (zipWith (<=) is (tail is))
+count l is = length (filter (l ==) is)
+double is = or (zipWith (\a b -> a == b && count a is == 2) is (tail is))
+
+solve :: (String -> Bool) -> (Int, Int) -> [Int]
+solve ok (lo, hi) = [pw | pw <- [lo..hi], ok (show pw)]
+
+check :: [String -> Bool] -> String -> Bool
+check preds i = all (\p -> p i) preds
+
+instance Challenge (Int, Int) where
+  parse _ = (134564, 585159)
+  partOne = show . length . solve (check [same, increase])
+  partTwo = show . length . solve (check [double, increase])
