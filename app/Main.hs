6bf8081efce8f1a03e2dc77a6520c6c834bc5c41
Bertalan Körmendy <korber314@gmail.com>
2021-02-07 10:20:30 +0100

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,11 @@ module Main where
 
 import Challenge
 import Intcode
-import Year2019.Day11
+import Year2019.Day13
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/11.txt"
+  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/13.txt"
   let parsed = parse input :: Program
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
