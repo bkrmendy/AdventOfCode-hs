6cebf2bfeee6c00f44e0037d5f718c2a62d626c4
Bertalan Körmendy <korber314@gmail.com>
2021-02-07 10:20:54 +0100

    Revert "wip"
    This reverts commit 6bf8081efce8f1a03e2dc77a6520c6c834bc5c41.

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,11 @@ module Main where
 
 import Challenge
 import Intcode
-import Year2019.Day13
+import Year2019.Day11
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/13.txt"
+  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/11.txt"
   let parsed = parse input :: Program
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
