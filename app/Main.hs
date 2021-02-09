a3cfa8cf6c3003c8249355cacba5ec1f2508d23f
Bertalan Körmendy <korber314@gmail.com>
2021-02-09 20:58:05 +0100

    qip

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,12 @@ module Main where
 
 import Challenge
 import Intcode
-import Year2019.Day15
+import Year2019.Day16
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/15.txt"
-  let parsed = parse input :: Program
+  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/16.txt"
+  let parsed = parse input :: [Int]
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
--- src/Year2019/Day16.hs
+++ src/Year2019/Day16.hs
@@ -1,2 +1,28 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Year2019.Day16 where
+import Challenge
+import Data.Char (digitToInt)
 
+patterns :: [[Int]]
+patterns = [generatePattern n | n <- [1..]]
+  where
+    basePattern = [0, 1, 0, -1] ++ basePattern
+    generatePattern n = drop 1 (concatMap (replicate n) basePattern)
+
+phase :: [Int] -> [Int] -> Int
+phase xs ys = (`mod` 10) $ abs $ sum $ zipWith (*) xs ys
+
+fft :: [Int] -> [[Int]]
+fft series = thisPhase:fft thisPhase
+  where
+    thisPhase = zipWith phase (replicate (length series) series) patterns
+
+partTwoI :: [String] -> [Int]
+partTwoI input = undefined
+    
+    
+
+instance Challenge [Int] where
+  parse = map digitToInt
+  partOne = concatMap show . take 8 . last . take 100 . fft
+  partTwo = show . length
