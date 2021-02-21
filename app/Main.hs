a9fb123935e4e587aef4950c781da1bb4e3f0ed5
Bertalan Körmendy <korber314@gmail.com>
2021-02-21 16:13:52 +0100

    get rid of absolute input file path

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE CPP #-}
 module Main where
 
 import Challenge
 import Intcode
 import Year2019.Day15
+import System.FilePath (takeDirectory, (</>))
+
+baseDir :: String
+baseDir = takeDirectory $ takeDirectory __FILE__
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile "/Users/prezi/IdeaProjects/aoc2016/input/2019/15.txt"
+  input <- readFile $ baseDir </> "input/2019/15.txt"
   let parsed = parse input :: Program
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
