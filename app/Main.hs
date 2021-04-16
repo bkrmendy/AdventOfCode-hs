434d57fd4a197201d46b844db20d7697f49d7515
Bertalan Körmendy <korber314@gmail.com>
2021-04-17 00:17:24 +0200

    2016 day 18

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 module Main where
 
 import Challenge
-import Year2015.Day17
+import Year2015.Day18
 import System.FilePath (takeDirectory, (</>))
 
 baseDir :: String
@@ -10,8 +10,8 @@ baseDir = takeDirectory $ takeDirectory __FILE__
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile $ baseDir </> "input/2015/17.txt"
-  let parsed = parse input :: [Int]
+  input <- readFile $ baseDir </> "input/2015/18.txt"
+  let parsed = parse input :: Garden
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
