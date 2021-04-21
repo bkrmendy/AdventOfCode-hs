e2844096e12d1a68f522a34475190096f7f9665d
Bertalan Körmendy <korber314@gmail.com>
2021-04-21 18:09:36 +0200

    multiple days complete

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 module Main where
 
 import Challenge
-import Year2017.Day10
+import Year2017.Day13
 import System.FilePath (takeDirectory, (</>))
 
 baseDir :: String
@@ -10,8 +10,8 @@ baseDir = takeDirectory $ takeDirectory __FILE__
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile $ baseDir </> "input/2017/10.txt"
-  let parsed = parse input :: String
+  input <- readFile $ baseDir </> "input/2017/13.txt"
+  let parsed = parse input :: Firewall
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
