c5ea8a1ca8c4e62ee3b5b03b2be2c2d847a5d262
Berci Kormendy <korber314@icloud.com>
2021-05-22 12:07:15 +0200

    2017 d 21, si

--- src/Year2017/Day21.hs
+++ src/Year2017/Day21.hs
@@ -8,57 +8,117 @@ import            Text.Parsec hiding (State)
 import qualified  Data.Map.Strict  as M
 import qualified  Data.Array       as A
 
-type Pattern = ([String], [String])
+type Pattern = ([String], String)
+
+patternChar :: Parsec String () Char
+patternChar = (char '.') <|> (char '#')
+
+sepBySlash :: Parsec String () [String]
+sepBySlash = sepBy1 (many1 patternChar) (char '/')
 
 pPattern :: Parsec String () Pattern
-pPattern = (,) <$> (sepBy1 (many1 anyChar) (char '/')) <*> (string " => " *> sepBy1 (many1 anyChar) (char '/'))
+pPattern = (,) <$> sepBySlash <*> (string " => " *> (concat <$> sepBySlash))
 
 type Grid = A.Array (Int, Int) Char
-type Book = M.Map [String] [String]
+type Book = M.Map String String
 
 rotate :: [[a]] -> [[a]]
 rotate = transpose . map reverse
 
-enhance :: Pattern -> [Pattern]
+rotations :: [String] -> [String]
+rotations pat =
+  let
+    r1 = rotate pat
+    r2 = rotate r1
+    r3 = rotate r2
+  in map concat [pat, r1, r2, r3]
+
+enhance :: Pattern -> [(String, String)]
 enhance (pattern, match) = go pattern ++ go (map reverse pattern)
-  where go = \pat -> let
-                        r1 = rotate pat
-                        r2 = rotate r1
-                        r3 = rotate r2
-                      in map ((,match)) [pat, r1, r2, r3]
+  where go = \pat -> map ((,match)) (rotations pat)
 
+matchFromBook :: Book -> String -> String
+matchFromBook book pattern = book M.! pattern
 
+lookupFromGrid :: Grid -> [(Int, Int)] -> String
+lookupFromGrid grid = map (grid A.!)
 
-step2 :: Book -> Grid -> Grid
-step2 = undefined
+getNextSquare :: Book -> Grid -> [(Int, Int)] -> [(Int, Int)] -> [((Int, Int), Char)]
+getNextSquare book grid fromCoords toCoords = zip toCoords (get fromCoords)
+  where get = matchFromBook book . lookupFromGrid grid
 
-step3 :: Book -> Grid -> Grid
-step3 = undefined
+square :: (Int, Int)    -- ^ top left column, top left row
+       -> Int           -- ^ side length
+       -> [(Int, Int)]  -- ^ coords
+square (tc, tr) side = [(c, r) | c <- [tc..tc + side - 1], r <- [tr..tr + side - 1]]
 
-squares :: Int -- ^ side length of generated square (2 or 3)
-        -> Int -- ^ number of generated squares
+squares :: Int -- ^ side length of generated square
+        -> Int -- ^ number of generated squares on side
         -> [[(Int, Int)]]
 squares side n = [go nc nr | nc <- [0..n - 1], nr <- [0..n - 1]]
   where
     go = \nc nr -> [(c, r) | c <- [nc*side..nc*side + side - 1], r <- [nr*side..nr*side + side - 1]]
 
+stepI :: Book -> Grid -> Int -> Int -> Grid
+stepI book grid dFrom dTo = nextGrid
+  where
+    (_, (width, _)) = A.bounds grid
+    sideSquares = (width + 1) `div` dFrom   -- ^ number of squares on one side in the from grid
+    squaresFrom = squares dFrom sideSquares -- ^ squares generated from the the from gird
+    squaresTo = squares dTo sideSquares     -- ^ squares generated from the the to gird
+    nextDim = sideSquares * dTo - 1         -- ^ next max dimension
+    nextsqrs = concatMap (uncurry (getNextSquare book grid)) (zip squaresFrom squaresTo)
+    nextGrid = A.array ((0, 0), (nextDim, nextDim)) nextsqrs
+
 step :: Book -> Grid -> Grid
-step book grid = if even (width + 1) then step2 book grid else step3 book grid
-  where (_, (width, _)) = A.bounds grid
+step book grid = nextGrid
+  where
+    (_, (width, _)) = A.bounds grid
+    nextGrid = if (even $ width + 1) then stepI book grid 2 3 else stepI book grid 3 4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 nPixelsOn :: Grid -> Int
 nPixelsOn = countWhere (== '#') . A.elems
   
 mkGrid :: Grid
-mkGrid = A.array ((0, 0), (2, 2)) [((c, r), (pattern !! r) !! c) | c <- [0, 1, 2], r <- [0, 1, 2]]
+mkGrid = A.array ((0, 0), (2, 2)) [((c, r), get c r) | c <- [0, 1, 2], r <- [0, 1, 2]]
   where
+    get = \c r -> (pattern !! r) !! c
     pattern = [ ".#."
               , "..#"
               , "###"
               ]     
 
+solve :: Int -> Book -> Int
+solve iterations book = nPixelsOn . last . take (iterations + 1) $ iterate (step book) mkGrid
+
 instance Challenge Book where
   parse = M.fromList . concatMap enhance . parseL pPattern
-  partOne book = show . nPixelsOn . last . take 5 $ iterate (step book) mkGrid
+  partOne = show . solve 5
+  partTwo = show . solve 18
 
 
