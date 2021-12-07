2bf584b1e600e390dda8f4c4737ea7b7ee9ad009
Berci Kormendy <korber314@icloud.com>
2021-12-07 08:04:02 +0100

    cleanup day 7

--- src/Year2021/Day7.hs
+++ src/Year2021/Day7.hs
@@ -3,21 +3,28 @@ module Year2021.Day7 where
 import Challenge
 import Utils (readInt)
 
+import Data.List (sort)
 import Data.List.Split (splitOn)
 
 type Fuel = Int -> Int -> Int
 
+tri :: Int -> Int
+tri n = n * (n + 1) `div` 2
+
+median :: [Int] -> Int
+median xs = sort xs !! (length xs `div` 2)
+
+mean :: [Int] -> Int
+mean xs = sum xs `div` length xs
+
 fuelPt1, fuelPt2 :: Fuel
 fuelPt1 from to = abs (to - from)
-fuelPt2 from to = sum $ map snd $ zip [a..b - 1] [1..]
-  where (a, b) = (min from to, max from to)
+fuelPt2 from to = tri $ abs (to - from)
 
-align :: Fuel -> [Int] -> Int
-align fuel positions = minimum $ map alignI [left .. right]
-  where (left, right) = (minimum positions, maximum positions)
-        alignI pos = sum $ map (fuel pos) positions
+alignTo :: Fuel -> Int -> [Int] -> Int
+alignTo fuel pos = sum . map (fuel pos)
 
 instance Challenge [Int] where
   parse = map readInt . splitOn ","
-  partOne = show . align fuelPt1
-  partTwo = show . align fuelPt2
+  partOne xs = show $ alignTo fuelPt1 (median xs) xs
+  partTwo xs = show $ alignTo fuelPt2 (mean xs) xs
