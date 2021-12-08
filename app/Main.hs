9d50d6e2d6bc677f00d7907c846c0f39bf461a80
Berci Kormendy <korber314@icloud.com>
2021-12-08 13:51:36 +0100

    day 8

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,15 @@ module Main where
 import System.FilePath (takeDirectory, (</>))
 
 import Challenge
-import Year2021.Day7
+import Year2021.Day8
 
 baseDir :: String
 baseDir = takeDirectory $ takeDirectory __FILE__
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile $ baseDir </> "input/2021/7.txt"
-  let parsed = parse input :: [Int]
+  input <- readFile $ baseDir </> "input/2021/8.txt"
+  let parsed = parse input :: [Entry]
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
---
+++ src/Year2021/Day8.hs
@@ -0,0 +1,86 @@
+module Year2021.Day8 where
+
+import Challenge
+  
+import Data.List (nub, sort)
+import Data.List.Split (splitOn)
+import qualified Data.Map.Strict as M
+
+import Control.Monad (guard)
+
+type Entry = ([String], [String])
+
+entry :: String -> Entry
+entry line = (sort <$> words code, sort <$> words ns)
+  where [code, ns] = splitOn " | " line
+
+type Mapping = M.Map String Int
+
+select :: [a] -> [(a, [a])]
+select [] = error "Cannot select from empty list"
+select [x] = [(x, [])]
+select (x:xs) = (x, xs):rest
+  where rest = [ (a, x:as) | (a, as) <- select xs]
+
+isSubsetOf :: (Eq a) => [a] -> [a] -> Bool
+a `isSubsetOf` b = all (`elem` b) a
+
+union :: (Eq a) => [a] -> [a] -> [a]
+a `union` b = nub (a ++ b)
+
+segments :: String -> Int -> Bool
+segments a n = length a == n
+
+mkMapping :: [String] -> Mapping
+mkMapping xs = M.fromList $ zipWith (\n a -> (a, n)) [0..] $ head $ do
+  (one, none) <- select xs
+  guard $ segments one 2
+  
+  (four, nfour) <- select none
+  guard $ segments four 4
+  
+  (seven, nseven) <- select nfour
+  guard $ segments seven 3
+  
+  (eight, neight) <- select nseven
+  guard $ segments eight 7
+  
+  (three, nthrees) <- select neight
+  guard $ segments three 5 && one `isSubsetOf` three
+  
+  (zero, nzeros) <- select nthrees
+  guard $ segments zero 6 && one `isSubsetOf` zero
+  
+  (nine, nnines) <- select nzeros
+  guard $ segments nine 6 && four `isSubsetOf` nine
+  
+  (five, nfives) <- select nnines
+  guard $ segments five 5 && five `isSubsetOf` nine
+  
+  (six, nsix) <- select nfives
+  guard $ segments six 6
+  
+  (two, _) <- select nsix
+  
+  pure [zero, one, two, three, four, five, six, seven, eight, nine]
+
+display :: Mapping -> [String] -> [Int]
+display mapping = map (mapping M.!)
+
+process :: Entry -> [Int]
+process (code, ns) = display (mkMapping code) ns
+
+partOneI :: [Entry] -> Int
+partOneI = sum . map (length . filter simple . process)
+  where simple a = a `elem` [1,4,7,8]
+  
+toInt :: [Int] -> Int
+toInt = foldl (\a n -> a * 10 + n) 0
+        
+partTwoI :: [Entry] -> Int
+partTwoI = sum . map (toInt . process) 
+
+instance Challenge [Entry] where
+  parse = map entry . lines
+  partOne = show . partOneI
+  partTwo = show . partTwoI
