ea7cbf91ca985770b7bfaaff49eb8ac9c621a435
Berci Kormendy <korber314@icloud.com>
2021-12-18 21:54:44 +0100

    d 13

--- app/Main.hs
+++ app/Main.hs
@@ -4,15 +4,15 @@ module Main where
 import System.FilePath (takeDirectory, (</>))
 
 import Challenge
-import Year2021.Day12
+import Year2021.Day13
 
 baseDir :: String
 baseDir = takeDirectory $ takeDirectory __FILE__
 
 runChallenge :: IO ()
 runChallenge = do
-  input <- readFile $ baseDir </> "input/2021/12.txt"
-  let parsed = parse input :: Graph
+  input <- readFile $ baseDir </> "input/2021/13.txt"
+  let parsed = parse input :: Manual
   putStrLn $ partOne parsed
   putStrLn $ partTwo parsed
 
